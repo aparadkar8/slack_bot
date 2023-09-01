c2932b8f8ad39fd52b657a71d36789c83b1a2bf7
anand <aparadkar@bestpeers.in>
2023-09-01 16:33:58 +0530

    modification in handle_payload action

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -5,11 +5,23 @@ class SlackController < ApplicationController
   end
 
   def handle_payload
-    payload = JSON.parse(request.body.read)
-    # Process the payload as needed
-    
-    response_message = payload['challenge']
-    
-    render json: response_message, status: :ok
+    debugger 
+    case params[:command]
+    when '/rootly'
+      case params[:text]
+      when /^declare\s+(.+)/i
+        incident_title = $1
+        payload = request.body.read
+        
+        # Parse other parameters (description, severity, etc.)
+        # Create incident in the database and Slack channel
+        # Send modal dialog to capture incident details
+      when 'resolve'
+        # Check if in dedicated incident channel
+        # Update incident status to resolved and calculate resolution time
+        # Send response back to channel
+      end
+    end
   end
+
 end
--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,3 +1,4 @@
 class Incident < ApplicationRecord
   validates :title, presence: true
+  validates :severity, inclusion: { in: %w(sev0 sev1 sev2)}
 end
